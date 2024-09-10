c160b3e4eadf865b31b53d988f38ab5054e9b83c
Erdinch <erdinchorhan02@gmail.com>
2024-09-11 01:27:40 +0300

    react-hook-form with zod for sign up

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,12 +1,92 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const User = z.object({
+  firstName: z
+    .string()
+    .trim()
+    .min(3, { message: 'First Name must be 3 or more characters long.' })
+    .max(14, {
+      message: 'First Name must be no longer than 14 characters.',
+    }),
+  lastName: z
+    .string()
+    .trim()
+    .min(3, { message: 'Last Name must be 3 or more characters long.' })
+    .max(14, {
+      message: 'Last Name must be no longer than 14 characters.',
+    }),
+  dateOfBirth: z.date({ message: 'Invalid date string!' }),
+  gender: z.enum(['male', 'female', 'other'], { message: 'Invalid gender.' }),
+  email: z
+    .string()
+    .trim()
+    .max(18, { message: "Email address can't be longer than 18 characters" })
+    .email({ message: 'Invalid email address.' }),
+  password: z
+    .string()
+    .trim()
+    .min(3, { message: 'Password must be 3 or more characters long.' })
+    .max(18, {
+      message: 'Password must be no longer than 18 characters.',
+    }),
+});
+
+type TFormInput = z.infer<typeof User>;
+
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TFormInput>({
+    resolver: zodResolver(User),
+  });
+
+  console.log(errors);
+
+  const onSubmit: SubmitHandler<TFormInput> = (data) => {
+    console.log('Form submitted');
+    console.log(data);
+  };
+
   return (
-    <div className="flex gap-2 flex-col py-12">
-      <input type="text" className="border-2" />
-      <input type="text" className="border-2" />
-      <input type="text" className="border-2" />
-      <input type="text" className="border-2" />
-      <input type="text" className="border-2" />
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col py-3 px-5">
+      <label>First Name</label>
+      <input
+        {...register('firstName')}
+        className="border-2 rounded-sm px-2 py-1"
+      />
+      {errors.firstName && <span>{errors.firstName.message}</span>}
+      <label>Last Name</label>
+      <input
+        {...register('lastName')}
+        className="border-2 rounded-sm px-2 py-1"
+      />
+      {errors.lastName && <span>{errors.lastName.message}</span>}
+      <label>Email</label>
+      <input {...register('email')} className="border-2 rounded-sm px-2 py-1" />
+      {errors.email && <span>{errors.email.message}</span>}
+      <label>Password</label>
+      <input
+        {...register('password')}
+        className="border-2 rounded-sm px-2 py-1"
+        type="password"
+      />
+      {errors.password && <span>{errors.password.message}</span>}
+      <label>Gender</label>
+      <select {...register('gender')} className="border-2 rounded-sm px-2 py-1">
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+        <option value="other">Other</option>
+      </select>
+      {errors.gender && <span>{errors.gender.message}</span>}
+      <input
+        type="submit"
+        className="self-end border-2 w-fit text-lg mt-3 px-3 py-1 border-gray-400 rounded-sm mb-2 hover:cursor-pointer hover:rounded-lg hover:bg-green-200 transition-all"
+      />
+    </form>
   );
 }
 
--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -7,12 +7,17 @@ function Auth() {
       <h1 className="text-green-500 font-bold text-9xl">Kquirtz</h1>
       <div className="flex shadow-md border-2 border-gray-50 rounded-md hover:border-gray-200 transition-all">
         <div className="px-6 flex gap-3">
-          <h2 className="text-green-400 font-bold text-4xl pt-4 whitespace-nowrap">
-            Sign Up!
-          </h2>
+          <div className="flex flex-col gap-3">
+            <h2 className="text-green-400 font-bold text-4xl pt-4">Sign Up!</h2>
+            <p className="text-gray-600">
+              Kquirtz is a social media web app that connects people!<br></br>
+              Share your best moments (or worst) <br></br> with the your social
+              circle, make new friends and keep up to date with them.
+            </p>
+          </div>
           <SignUp />
         </div>
-        <div className="bg-[#1a1a1a] rounded-tr-md rounded-br-md shadow-md w-[170px] text-white px-8 flex justify-center items-center">
+        <div className="bg-[#1a1a1a] rounded-tr-md rounded-br-md shadow-md basis-2/4 text-white px-8 flex justify-center items-center">
           <h2 className="font-bold text-2xl">Log In!</h2>
           {/* <LogIn /> */}
         </div>
