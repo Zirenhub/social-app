e783cb6850a589883aa7d182aebe04aaedf88637
Erdinch <erdinchorhan02@gmail.com>
2024-09-21 03:14:45 +0300

    added username and bio plus helper funcs

--- client/src/components/auth/SignUpUserSchema.ts
+++ client/src/components/auth/SignUpUserSchema.ts
@@ -1,21 +1,30 @@
 import { z } from 'zod';
 
+function getMinCharError(field: string, minChar: number) {
+  return { message: `${field} must be ${minChar} or more characters long.` };
+}
+
+function getMaxCharError(field: string, maxChar: number) {
+  return { message: `${field} must be no longer than ${maxChar} characters.` };
+}
+
 const User = z
   .object({
+    username: z
+      .string()
+      .trim()
+      .min(3, getMinCharError('Username', 3))
+      .max(14, getMaxCharError('Username', 14)),
     firstName: z
       .string()
       .trim()
-      .min(3, { message: 'First Name must be 3 or more characters long.' })
-      .max(14, {
-        message: 'First Name must be no longer than 14 characters.',
-      }),
+      .min(3, getMinCharError('First Name', 3))
+      .max(14, getMaxCharError('First Name', 14)),
     lastName: z
       .string()
       .trim()
-      .min(3, { message: 'Last Name must be 3 or more characters long.' })
-      .max(14, {
-        message: 'Last Name must be no longer than 14 characters.',
-      }),
+      .min(3, getMinCharError('Last Name', 3))
+      .max(14, getMaxCharError('Last Name', 14)),
     day: z.number().min(1, 'Day is required').max(31),
     month: z.number().min(0, 'Month is required').max(11),
     year: z
@@ -23,6 +32,7 @@ const User = z
       .min(1900, 'Year is required')
       .max(new Date().getFullYear()),
     gender: z.enum(['male', 'female', 'other'], { message: 'Invalid gender.' }),
+    bio: z.string().trim().max(160, getMaxCharError('Bio', 160)),
     email: z
       .string()
       .trim()
@@ -31,10 +41,8 @@ const User = z
     password: z
       .string()
       .trim()
-      .min(3, { message: 'Password must be 3 or more characters long.' })
-      .max(18, {
-        message: 'Password must be no longer than 18 characters.',
-      }),
+      .min(3, getMinCharError('Password', 3))
+      .max(18, getMaxCharError('Password', 18)),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
