3ddefa132cd20fc87b1d3a1e114e95716c5bdbcf
Erdinch <erdinchorhan02@gmail.com>
2024-09-23 02:35:35 +0300

    response handler

---
+++ server/src/utils/responseHandler.ts
@@ -0,0 +1,15 @@
+import { Response } from "express";
+import { ApiResponse } from "../types/apiResponse";
+
+export function sendSuccessResponse<T>(
+  res: Response,
+  data: T,
+  status: number = 200
+) {
+  const response: ApiResponse<T> = {
+    success: true,
+    data,
+    error: null,
+  };
+  res.status(status).json(response);
+}
