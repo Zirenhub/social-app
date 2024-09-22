7672a64ee4ae8fe85ad22cf944be0c0fa307d5f5
Erdinch <erdinchorhan02@gmail.com>
2024-09-23 02:34:52 +0300

    prisma init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import http from "http";
 import app from "./app";
+import prisma from "./prisma";
 
 dotenv.config();
 
@@ -10,4 +11,14 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Gracefully close Prisma Client on shutdown
+process.on("SIGINT", async () => {
+  console.log("Shutting down server...");
+  await prisma.$disconnect();
+  server.close(() => {
+    console.log("Server closed");
+    process.exit(0);
+  });
 });
