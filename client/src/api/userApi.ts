788c2e1b83db3373efc4da63d945b81e5c0b70d3
Erdinch <erdinchorhan02@gmail.com>
2024-09-30 02:58:00 +0300

    whoami endpoint

--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -1,26 +1,25 @@
 import api from '../app/axios';
 import z from 'zod';
-import { UserSignUp, UserLogIn, ApiResponse } from 'shared';
-import {
-  LoginResponse,
-  LogOutResponse,
-  SignUpResponse,
-} from '../types/api/auth';
+import { UserSignUp, UserLogIn, ApiResponse, TAuthUser } from 'shared';
+import { LogOutResponse } from '../types/api/auth';
 import { errorHandler } from './errorHandler';
 
 type TLogInData = z.infer<typeof UserLogIn>;
 type TSignUpData = z.infer<typeof UserSignUp>;
 
 // API functions
+const whoAmIBase = () => api.get<ApiResponse<TAuthUser>>('auth/whoami');
+
 const logInApiBase = (data: TLogInData) =>
-  api.post<ApiResponse<LoginResponse>>('auth/login', data);
+  api.post<ApiResponse<TAuthUser>>('auth/login', data);
 
 const signupApiBase = (data: TSignUpData) =>
-  api.post<ApiResponse<SignUpResponse>>('auth/signup', data);
+  api.post<ApiResponse<TAuthUser>>('auth/signup', data);
 
+const whoAmiApi = errorHandler(whoAmIBase);
 const logInApi = errorHandler(logInApiBase);
 const signupApi = errorHandler(signupApiBase);
 
 const logoutApi = async () => await api.post<LogOutResponse>('auth/logout');
 
-export { logInApi, signupApi, logoutApi };
+export { whoAmiApi, logInApi, signupApi, logoutApi };
