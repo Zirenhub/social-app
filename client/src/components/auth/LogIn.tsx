07e811e6a10cef97467a7376694b317d51be168d
Erdinch <erdinchorhan02@gmail.com>
2024-09-29 18:52:55 +0300

    login component done

--- client/src/components/auth/LogIn.tsx
+++ client/src/components/auth/LogIn.tsx
@@ -1,12 +1,40 @@
+import { useEffect } from 'react';
+import { useLogInForm } from './helpers/useLogInForm';
+import { toast } from 'react-toastify';
+
 function LogIn() {
+  const { submit, formMethods, formErrors, apiError } = useLogInForm();
+
+  useEffect(() => {
+    const errorMessages = Object.values(formErrors).map(
+      (error) => error?.message
+    );
+    errorMessages.forEach((message) => {
+      if (message) toast.error(message, { toastId: message });
+    });
+  }, [formErrors]);
+
+  useEffect(() => {
+    if (apiError) toast.error(apiError?.message);
+  }, [apiError]);
+
   return (
-    <div className="flex flex-col">
+    <form onSubmit={submit} className="flex flex-col">
       <label>Email</label>
-      <input type="text" className="input-auth" />
+      <input
+        {...formMethods.register('email')}
+        type="text"
+        className="input-auth"
+      />
       <label>Password</label>
-      <input type="text" className="input-auth" />
+      <input
+        {...formMethods.register('password')}
+        type="password"
+        autoComplete="new-password"
+        className="input-auth"
+      />
       <button className="btn-submit">Log In</button>
-    </div>
+    </form>
   );
 }
 
