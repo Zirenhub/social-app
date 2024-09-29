47efca55b4f295d0fc70412a88b6e7d9627fd6a4
Erdinch <erdinchorhan02@gmail.com>
2024-09-29 03:47:56 +0300

    removed unused duplicate, added new app error messages

--- server/src/constants/constant.ts
+++ server/src/constants/constant.ts
@@ -8,15 +8,6 @@ export const HTTP_RESPONSE_CODE = {
   SERVER_ERROR: 500,
 };
 
-export const enum HttpStatusCode {
-  NOT_FOUND = 404,
-  CREATED = 201,
-  CONFLICT = 409,
-  BAD_REQUEST = 400,
-  SUCCESS = 200,
-  UNAUTHORIZED = 401,
-}
-
 export const APP_ERROR_MESSAGE = {
   serverError: "Something went wrong, try again later",
   createdUser: "User created successfully",
@@ -28,7 +19,8 @@ export const APP_ERROR_MESSAGE = {
   eventsReturned: "Events Returned successfully",
   reviewsReturned: "Reviews Returned successfully",
   userDoesntExist: "User does not exist",
-  eventDoesntExist: "Event does not exist",
+  contentDoesntExist: "Content does not exist",
   invalidCredentials: "Invalid user email or password",
   invalidEmail: "Enter a valid email address",
+  uniqueConstraintViolation: "A unique constraint violation occurred.",
 };
