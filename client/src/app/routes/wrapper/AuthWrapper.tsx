a1a681e3c65de5fc0e1d148381ca2806f1d4f1e0
Erdinch <erdinchorhan02@gmail.com>
2024-09-30 02:59:47 +0300

    auth wrapper if jwt is valid, show index child on route, if not show auth

---
+++ client/src/app/routes/wrapper/AuthWrapper.tsx
@@ -0,0 +1,32 @@
+import { Outlet } from 'react-router-dom';
+import useAuthStore from '../../../stores/authStore';
+import Auth from '../../pages/auth/Auth';
+import { useQuery } from '@tanstack/react-query';
+import { whoAmiApi } from '../../../api/userApi';
+import { useEffect } from 'react';
+
+const AuthWrapper = () => {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const setUser = useAuthStore((state) => state.setUser);
+
+  const { data, error } = useQuery({
+    queryKey: ['whoami'],
+    queryFn: whoAmiApi,
+    retry: false, // Default is retry for 3 times if getting error from server
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setUser(data);
+    }
+  }, [data, error, setUser]);
+
+  if (!isAuthenticated) {
+    return <Auth />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthWrapper;
