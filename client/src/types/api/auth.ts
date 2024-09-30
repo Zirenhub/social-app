de1463842c94843d36b33d7fb10f2448282ccbfe
Erdinch <erdinchorhan02@gmail.com>
2024-09-30 03:04:31 +0300

    fixed type in api

--- client/src/types/api/auth.ts
+++ client/src/types/api/auth.ts
@@ -1,12 +1,4 @@
-import { ApiResponse, TAuthUser } from 'shared';
-
-export interface LoginResponse extends ApiResponse<unknown> {
-  data: TAuthUser;
-}
-
-export interface SignUpResponse extends ApiResponse<unknown> {
-  data: TAuthUser;
-}
+import { ApiResponse } from 'shared';
 
 export interface LogOutResponse extends ApiResponse<unknown> {
   success: true;
