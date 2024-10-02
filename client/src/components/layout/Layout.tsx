45d8356aa0f2794f7acadaaaf0ded63fa2cea56a
Erdinch <erdinchorhan02@gmail.com>
2024-10-02 03:41:58 +0300

    seperated nav from layout

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,30 +1,13 @@
-import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import useAuthStore, { useLogout } from '../../stores/authStore';
+import { Outlet, useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import {
-  modalVariants,
   slideAboveVariants,
   slideLeftVariants,
 } from '../../constants/constants';
-import { toast } from 'react-toastify';
+import Navigation from './Navigation';
 
 const Layout = () => {
-  const [userOptionsModal, setUserOptionsModal] = useState<boolean>(false);
-  const user = useAuthStore((state) => state.user);
-
   const location = useLocation();
-  const navigate = useNavigate();
-  const navLinks = [
-    { value: 'Home', to: '/' },
-    { value: 'Profile', to: 'profile' },
-  ];
-
-  const { mutate: logout, error } = useLogout();
-
-  useEffect(() => {
-    if (error) toast.error(error.message);
-  }, [error]);
 
   return (
     <motion.div
@@ -35,62 +18,9 @@ const Layout = () => {
       transition={{ duration: 0.5, ease: 'easeInOut' }}
       className="flex-1 flex backdrop-blur-lg"
     >
-      <nav className="basis-1/5 flex flex-col justify-between bg-third/75 shadow-lg rounded-xl m-[2px]">
-        <ul className="flex flex-col gap-3 m-4">
-          {navLinks.map((link) => (
-            <li
-              key={link.value}
-              onClick={() => navigate(link.to)}
-              className="text-2xl hover:underline underline-offset-4 bg-primary text-fourth shadow-md px-4 py-3 rounded-full cursor-pointer transition-all transform hover:scale-105"
-            >
-              <Link to={link.to}>{link.value}</Link>
-            </li>
-          ))}
-        </ul>
+      <Navigation />
 
-        <div
-          onClick={() => setUserOptionsModal(!userOptionsModal)}
-          className={`flex justify-between items-center cursor-pointer select-none rounded-lg relative m-4 p-3 transition-all bg-secondary/80 ${userOptionsModal && 'bg-[#664343]'} text-white shadow-md backdrop-blur-md`}
-        >
-          <AnimatePresence>
-            {userOptionsModal && (
-              <motion.div
-                variants={modalVariants}
-                key={'modal'}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                className="bg-fourth text-primary -top-[15%] left-1/2 -translate-x-1/2 -translate-y-1/2 absolute py-2 px-4 rounded-lg z-10 shadow-xl"
-              >
-                <button
-                  className="font-bold hover:underline underline-offset-4 transition-transform transform hover:scale-105"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    logout({});
-                  }}
-                >
-                  Log Out @{user?.profile.username}
-                </button>
-                <div className="absolute top-[25px] left-3/4">&#9660;</div>
-              </motion.div>
-            )}
-          </AnimatePresence>
-          <div className="flex gap-4 items-center">
-            <div className="w-16 h-16 rounded-full bg-default-pfp bg-cover bg-center border-2 border-secondary shadow-lg"></div>
-            <div className="flex flex-col">
-              <span className="text-lg font-bold text-primary truncate">
-                {user?.profile.firstName} {user?.profile.lastName}
-              </span>
-              <span className="text-sm text-primary">
-                @{user?.profile.username}
-              </span>
-            </div>
-          </div>
-          <div className="bg-ellipsis bg-cover bg-center h-4 w-4 mr-3"></div>
-        </div>
-      </nav>
-
-      <main className="flex-grow p-2 bg-primary text-fourth shadow-inner rounded-xl m-[2px]">
+      <main className="flex-grow p-2 bg-primary text-fourth shadow-inner rounded-xl m-1">
         <motion.div
           key={location.pathname}
           initial="initial"
@@ -98,7 +28,7 @@ const Layout = () => {
           exit="exit"
           variants={slideAboveVariants}
           transition={{ duration: 0.5, ease: 'easeInOut' }}
-          className="p-5 bg-white/70 backdrop-blur-md shadow-xl rounded-lg"
+          className="bg-white/70 backdrop-blur-md shadow-xl rounded-lg"
         >
           <Outlet />
         </motion.div>
---
+++ client/src/components/layout/Navigation.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react';
+import useAuthStore, { useLogout } from '../../stores/authStore';
+import { toast } from 'react-toastify';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import { modalVariants } from '../../constants/constants';
+
+function Navigation() {
+  const [userOptionsModal, setUserOptionsModal] = useState<boolean>(false);
+  const user = useAuthStore((state) => state.user);
+
+  const { mutate: logout, error } = useLogout();
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const navLinks = [
+    { value: 'Home', to: '/', svg: 'bg-home' },
+    { value: 'Profile', to: 'profile', svg: 'bg-profile' },
+  ];
+
+  const isCurrentPath = (path: string) => {
+    // pathname starts with "/" react router link's to property must start without /
+    // remove / if we are not at home and check if path param equals to pathname
+    // doing this to hightlight the link button for the current path
+    if (pathname === '/' && path === '/') {
+      return true;
+    } else if (pathname !== '/') {
+      return pathname.slice(1) === path;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (error) toast.error(error.message);
+  }, [error]);
+
+  return (
+    <nav className="basis-1/5 flex flex-col justify-between bg-third/75 shadow-lg rounded-xl m-1">
+      <ul className="flex flex-col gap-3 m-4">
+        {navLinks.map((link) => (
+          <li
+            key={link.value}
+            onClick={() => navigate(link.to)}
+            className="text-2xl flex items-center gap-3 hover:underline underline-offset-4 bg-primary text-fourth shadow-md px-4 py-3 rounded-full cursor-pointer transition-all transform hover:scale-105"
+          >
+            <div className={`w-8 h-8 ${link.svg} bg-cover bg-center`} />
+            <Link
+              to={link.to}
+              className={isCurrentPath(link.to) ? 'font-bold' : ''}
+            >
+              {link.value}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setUserOptionsModal(!userOptionsModal)}
+        className={`flex justify-between items-center cursor-pointer select-none rounded-lg relative m-4 p-3 transition-all bg-secondary/80 ${userOptionsModal && 'bg-[#664343]'} text-white shadow-md backdrop-blur-md`}
+      >
+        <AnimatePresence>
+          {userOptionsModal && (
+            <motion.div
+              variants={modalVariants}
+              key={'modal'}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              className="bg-fourth text-primary -top-[15%] left-1/2 -translate-x-1/2 -translate-y-1/2 absolute py-2 px-4 rounded-lg z-10 shadow-xl"
+            >
+              <button
+                className="font-bold hover:underline underline-offset-4 transition-transform transform hover:scale-105"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  logout({});
+                }}
+              >
+                Log Out @{user?.profile.username}
+              </button>
+              <div className="absolute top-[25px] left-3/4">&#9660;</div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <div className="flex gap-4 items-center">
+          <div className="w-16 h-16 rounded-full bg-default-pfp bg-cover bg-center border-2 border-secondary shadow-lg"></div>
+          <div className="flex flex-col">
+            <span className="text-lg font-bold text-primary truncate">
+              {user?.profile.firstName} {user?.profile.lastName}
+            </span>
+            <span className="text-sm text-primary">
+              @{user?.profile.username}
+            </span>
+          </div>
+        </div>
+        <div className="bg-ellipsis bg-cover bg-center h-4 w-4 mr-3"></div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
