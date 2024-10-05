72b21f699cbfb40c6792c267d4c98c9d2e334df6
Erdinch <erdinchorhan02@gmail.com>
2024-10-06 01:44:19 +0300

    post friend request action

--- server/src/controllers/profileController.ts
+++ server/src/controllers/profileController.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import client from "../prisma";
 import { sendSuccessResponse } from "../utils/responseHandler";
+import { HttpException } from "../exceptions/error";
+import { HTTP_RESPONSE_CODE } from "../constants/constant";
 
 const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -15,4 +17,35 @@ const get = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default { get };
+const friendRequest = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const requestUsername = req.params.username;
+    const requestUser = await client.profile.findUniqueOrThrow({
+      where: { username: requestUsername },
+      omit: { userId: true },
+    });
+    const currentUserId = req.user?.id; // should never be undefined as we are checking it inside verifyJWT middleware
+    if (!currentUserId) {
+      throw new HttpException(
+        HTTP_RESPONSE_CODE.BAD_REQUEST,
+        "Invalid username passed."
+      );
+    }
+    const friendshipRequest = await client.friendRequest.create({
+      data: {
+        status: "PENDING",
+        senderId: currentUserId,
+        receiverId: requestUser.id,
+      },
+    });
+    sendSuccessResponse(res, friendshipRequest);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { get, friendRequest };
--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -5,5 +5,6 @@ import profileController from "../controllers/profileController";
 const profileRouter: Router = Router();
 
 profileRouter.get("/:username", verifyJwt, profileController.get);
+profileRouter.post("/:username", verifyJwt, profileController.friendRequest);
 
 export { profileRouter };
