4b650f8b581e48c28b6f5680f3a6f1689c0eb6c6
Erdinch <erdinchorhan02@gmail.com>
2024-10-08 00:42:07 +0300

    friend request, add, delete

--- client/src/api/profileApi.ts
+++ client/src/api/profileApi.ts
@@ -1,10 +1,16 @@
-import { ApiResponse, TProfileApi } from 'shared';
+import { ApiResponse, FriendRequest, TProfileApi } from 'shared';
 import api from '../app/axios';
 import { errorHandler } from './errorHandler';
 
 const getProfileBase = (username: string) =>
   api.get<ApiResponse<TProfileApi>>(`profile/${username}`);
+const postFriendshipRequestBase = (username: string) =>
+  api.post<ApiResponse<FriendRequest>>(`/profile/${username}/friend-request`);
+const deleteFriendshipRequestBase = (username: string) =>
+  api.delete<ApiResponse<null>>(`/profile/${username}/friend-request`);
 
 const getProfileApi = errorHandler(getProfileBase);
+const postFriendshipRequestApi = errorHandler(postFriendshipRequestBase);
+const deleteFriendshipRequestApi = errorHandler(deleteFriendshipRequestBase);
 
-export { getProfileApi };
+export { getProfileApi, postFriendshipRequestApi, deleteFriendshipRequestApi };
--- client/src/stores/profileStore.ts
+++ client/src/stores/profileStore.ts
@@ -1,14 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
-import { TProfileApi } from 'shared';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { TFriendshipStatus, TProfileApi } from 'shared';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
-import { getProfileApi } from '../api/profileApi';
+import {
+  deleteFriendshipRequestApi,
+  getProfileApi,
+  postFriendshipRequestApi,
+} from '../api/profileApi';
 import { ApiError } from '../api/error';
 
 type TProfileStore = {
   profile: TProfileApi | null;
   isMyProfile: boolean;
   setProfile: (profile: TProfileApi, currentUsername: string) => void;
+  setFriendshipStatus: (friendRequest: TFriendshipStatus) => void;
 };
 
 const useProfileStore = create<TProfileStore>()(
@@ -16,7 +21,14 @@ const useProfileStore = create<TProfileStore>()(
     profile: null,
     isMyProfile: false,
     setProfile: (profile: TProfileApi, currentUsername: string) =>
-      set({ profile, isMyProfile: profile.username === currentUsername }),
+      set({
+        profile,
+        isMyProfile: profile.username === currentUsername,
+      }),
+    setFriendshipStatus: (friendshipStatus: TFriendshipStatus) =>
+      set((state) => ({
+        profile: state.profile ? { ...state.profile, friendshipStatus } : null,
+      })),
   }))
 );
 
@@ -25,6 +37,48 @@ export const useProfileQuery = (username: string) => {
     queryKey: ['profile', username],
     queryFn: () => getProfileApi(username),
     retry: false,
+  });
+};
+
+export const useFriendshipRequestMutation = (
+  username: string,
+  successCallback: () => void,
+  errorCallback: (errorMsg: string) => void
+) => {
+  const { setFriendshipStatus } = useProfileStore();
+
+  return useMutation({
+    mutationKey: ['profile', username],
+    mutationFn: () => postFriendshipRequestApi(username),
+    onSuccess(data) {
+      setFriendshipStatus({ status: 'RECEIVED_REQUEST', requestId: data.id });
+      successCallback();
+    },
+    onError(err) {
+      console.log(err);
+      errorCallback(err.message);
+    },
+  });
+};
+
+export const useDeleteFriendshipRequestMutation = (
+  username: string,
+  successCallback: () => void,
+  errorCallback: (errorMsg: string) => void
+) => {
+  const { setFriendshipStatus } = useProfileStore();
+
+  return useMutation({
+    mutationKey: ['profile', username],
+    mutationFn: () => deleteFriendshipRequestApi(username),
+    onSuccess() {
+      setFriendshipStatus({ status: 'NOT_FRIENDS', requestId: null });
+      successCallback();
+    },
+    onError(err) {
+      console.log(err);
+      errorCallback(err.message);
+    },
   });
 };
 
