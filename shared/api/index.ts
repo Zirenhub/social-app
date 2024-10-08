a1da20965286ac735a5bef4b0b51ecfd8bfcf5ce
Erdinch <erdinchorhan02@gmail.com>
2024-10-09 02:45:02 +0300

    tidy up types

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -1,26 +1,34 @@
 import type { ApiResponse } from "./apiResponse";
-import { User, Profile, Post, Like, Comment } from "../types";
+import { User, Profile, Post, Like, Comment, FriendRequest } from "../types";
 
-type TAuthUserApi = Omit<User, "passwordHash"> & { profile: Profile };
-type TPostApi = Post & {
-  profile: Omit<Profile, "userId">;
-  likes: Like[];
-  comments: Comment[];
-};
-
+type TProfileBase = Omit<Profile, "userId">;
 type EFriendshipStatus =
   | "FRIENDS"
   | "RECEIVED_REQUEST"
   | "SENT_REQUEST"
   | "NOT_FRIENDS";
-
 type TFriendshipStatus = {
   status: EFriendshipStatus;
   requestId: number | null;
 };
 
+// types of the responses, responded by api
+type TAuthUserApi = Omit<User, "passwordHash"> & { profile: Profile };
+type TPostApi = Post & {
+  profile: TProfileBase;
+  likes: Like[];
+  comments: Comment[];
+};
 type TProfileApi = Omit<Profile, "userId"> & {
   friendshipStatus: TFriendshipStatus;
 };
+type TFriendRequestApi = FriendRequest & { sender: TProfileBase };
 
-export { ApiResponse, TAuthUserApi, TProfileApi, TPostApi, TFriendshipStatus };
+export {
+  ApiResponse,
+  TAuthUserApi,
+  TProfileApi,
+  TPostApi,
+  TFriendshipStatus,
+  TFriendRequestApi,
+};
