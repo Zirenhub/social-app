c03241a10b4d348a09020e0ed631430c1e1916d9
Erdinch <erdinchorhan02@gmail.com>
2024-10-24 19:18:45 +0300

    create post on home page

--- client/src/app/pages/home/Home.tsx
+++ client/src/app/pages/home/Home.tsx
@@ -2,20 +2,31 @@ import IsLoading from '../../../components/IsLoading';
 import ShowError from '../../../components/ShowError';
 import { useGetAllPostsQuery } from '../../../stores/postsStore';
 import PostContainer from '../../../components/post/PostContainer';
+import CreatePost from '../../../components/post/CreatePost';
 
 function Home() {
   const { data, isLoading, error } = useGetAllPostsQuery();
 
   if (isLoading) {
-    <IsLoading />;
+    return <IsLoading />;
   }
 
   if (error) {
-    <ShowError message={error.message} />;
+    return <ShowError message={error.message} />;
   }
 
-  if (data && data.length) {
-    return data.map((post) => <PostContainer key={post.id} post={post} />);
+  if (data?.length) {
+    return (
+      <div className="flex flex-col">
+        <div className=" text-secondary rounded-br-lg p-3">
+          <CreatePost />
+        </div>
+        <div className="h-1 bg-third/20 mt-2" />
+        {data.map((post) => (
+          <PostContainer key={post.id} post={post} />
+        ))}
+      </div>
+    );
   }
 
   return <div>No posts to show</div>;
